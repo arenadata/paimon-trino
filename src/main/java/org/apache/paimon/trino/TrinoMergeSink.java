eacd541bd8d62bff51cc4532da0a235c376262f3
Nikita Ryanov <ryanov.nikita@gmail.com>
2026-07-02 18:30:15 +0300

    fix: correctly handle UPDATE/DELETE

--- src/main/java/org/apache/paimon/trino/TrinoMergeSink.java
+++ src/main/java/org/apache/paimon/trino/TrinoMergeSink.java
@@ -22,15 +22,12 @@ import org.apache.paimon.types.RowKind;
 
 import io.airlift.slice.Slice;
 import io.trino.spi.Page;
-import io.trino.spi.block.Block;
 import io.trino.spi.connector.ConnectorMergeSink;
 import io.trino.spi.connector.ConnectorPageSink;
-import io.trino.spi.type.TinyintType;
+import io.trino.spi.connector.MergePage;
 
 import java.util.Collection;
-import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
-import java.util.stream.IntStream;
 
 /** Trino {@link ConnectorMergeSink}. */
 public class TrinoMergeSink implements ConnectorMergeSink {
@@ -45,64 +42,13 @@ public class TrinoMergeSink implements ConnectorMergeSink {
 
     @Override
     public void storeMergedRows(Page page) {
-        int inputChannelCount = page.getChannelCount();
-        if (inputChannelCount != dataColumnCount + 2) {
-            throw new IllegalArgumentException(
-                    String.format(
-                            "inputPage channelCount (%s) == dataColumns size (%s) + 2",
-                            inputChannelCount, dataColumnCount));
-        } else {
-            int positionCount = page.getPositionCount();
-            if (positionCount <= 0) {
-                throw new IllegalArgumentException(
-                        "positionCount should be > 0, but is " + positionCount);
-            } else {
-                Block operationBlock = page.getBlock(inputChannelCount - 2);
-                int[] deletePositions = new int[positionCount];
-                int[] insertPositions = new int[positionCount];
-                int deletePositionCount = 0;
-                int insertPositionCount = 0;
-
-                for (int position = 0; position < positionCount; ++position) {
-                    byte operation = TinyintType.TINYINT.getByte(operationBlock, position);
-                    switch (operation) {
-                        case 1:
-                        case 4:
-                            insertPositions[insertPositionCount] = position;
-                            ++insertPositionCount;
-                            break;
-                        case 2:
-                        case 5:
-                            deletePositions[deletePositionCount] = position;
-                            ++deletePositionCount;
-                            break;
-                        case 3:
-                        default:
-                            throw new IllegalArgumentException(
-                                    "Invalid merge operation: " + operation);
-                    }
-                }
-
-                Optional<Page> deletePage = Optional.empty();
-                if (deletePositionCount > 0) {
-                    deletePage =
-                            Optional.of(
-                                    page.getColumns(IntStream.range(0, dataColumnCount).toArray())
-                                            .getPositions(deletePositions, 0, deletePositionCount));
-                }
-
-                Optional<Page> insertPage = Optional.empty();
-                if (insertPositionCount > 0) {
-                    insertPage =
-                            Optional.of(
-                                    page.getColumns(IntStream.range(0, dataColumnCount).toArray())
-                                            .getPositions(insertPositions, 0, insertPositionCount));
-                }
-
-                deletePage.ifPresent(delete -> pageSink.writePage(delete, RowKind.DELETE));
-                insertPage.ifPresent(insert -> pageSink.writePage(insert, RowKind.INSERT));
-            }
-        }
+        MergePage mergePage = MergePage.createDeleteAndInsertPages(page, dataColumnCount);
+        mergePage
+                .getDeletionsPage()
+                .ifPresent(deletePage -> pageSink.writePage(deletePage, RowKind.DELETE));
+        mergePage
+                .getInsertionsPage()
+                .ifPresent(insertPage -> pageSink.writePage(insertPage, RowKind.INSERT));
     }
 
     @Override
